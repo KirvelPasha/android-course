f89b72fb7b426b7eac45e5f721206785fe3515d0
Pasha <kirvelpasha99@gmail.com>
2019-09-25 22:39:49 +0300

    fixed bugs

--- lab(1)/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ lab(1)/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.example.myapplication
 
-import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.view.*
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 const val MESSAGE = "com.example.myapplication"
 class MainActivity : AppCompatActivity() {
 
@@ -18,7 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 2) {
+        if (requestCode == 1) {
             val answer = data?.getStringExtra(MESSAGE)
             val textView = findViewById<TextView>(R.id.textView).apply {
                 text = answer
@@ -27,8 +26,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun sendQuestion(view : View) {
-        val editText = findViewById<EditText>(R.id.editText)
-        val question = editText.text.toString()
+        val editText = editText.text
+        val question = editText.toString()
         val intent = Intent(this,SecondActivity::class.java).apply {
             putExtra(MESSAGE,question)
         }
--- lab(1)/app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ lab(1)/app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -2,39 +2,28 @@ package com.example.myapplication
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_second.*
 
 
 class SecondActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        textView2.text = intent.getStringExtra(MESSAGE)
     }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 1) {
-            val question = intent.getStringExtra(MESSAGE)
-            val textView = findViewById<TextView>(R.id.textView2).apply {
-                text = question
-            }
-        }
-    }
-
 
     fun sendAnswer(view : View) {
-        val editText = findViewById<EditText>(R.id.editText2)
-        val answer = editText.text.toString()
+        val editText = editText2.text
+        val answer = editText.toString()
         val intent = Intent(this,MainActivity::class.java).apply {
             putExtra(MESSAGE,answer)
         }
-        startActivityForResult(intent,2)
+        setResult(Activity.RESULT_OK,intent)
+        finish()
     }
 
 }
