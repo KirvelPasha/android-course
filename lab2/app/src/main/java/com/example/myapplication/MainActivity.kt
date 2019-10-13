afb49f78fb41a2aac2baa200103168593f66560b
Pasha <kirvelpasha99@gmail.com>
2019-10-13 20:10:46 +0300

    update

--- lab2/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ lab2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
+
+const val MESSAGE = "com.example.myapplication"
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1) {
+            textAnswer.text = data?.getStringExtra(MESSAGE)
+        }
+    }
+
+    fun sendQuestion(view : View) {
+        val intent = Intent(this,SecondActivity::class.java).apply {
+            putExtra(MESSAGE,editQuestion.text.toString())
+        }
+        startActivityForResult(intent,1)
     }
 }
---
+++ lab2/app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        textQuestion.text = intent.getStringExtra(MESSAGE)
+    }
+
+    fun sendAnswer(view : View) {
+        val intent = Intent(this,MainActivity::class.java).apply {
+            putExtra(MESSAGE,editAnswer.text.toString())
+        }
+        setResult(Activity.RESULT_OK,intent)
+        finish()
+    }
+}
